0acff32b5e19ebc07b37317b17a359bf7494bd4f
ViktorBoiarchuk <victorboiarchuk@gmail.com>
2020-11-28 22:03:43 +0200

    Update task4.py

--- sprint3/task4.py
+++ sprint3/task4.py
@@ -1,8 +1,6 @@
 def prime_factors(N):
-   # while True:
-    try:
-        for x in range(1, N+1):
-            if (N % x == 0):
-                yield x
-    except StopIteration:
-        return ?
+    for x in range(1, N+1):
+        if N % x == 0:
+            yield x
+    while True:
+        yield None
