145e75660b493d879823b1f2698e8825241a3416
ViktorBoiarchuk <victorboiarchuk@gmail.com>
2020-11-28 20:39:57 +0200

    Update task3_possible_solution.py
    add secrets validation logic

--- sprint3/task3_possible_solution.py
+++ sprint3/task3_possible_solution.py
@@ -10,14 +10,25 @@ def validate_pwd(pwd: str):
         raise ValueError('Password does not match its requirements. Check them out, please.')
 
 
+def are_secrets_valid(base_secrets, secrets_to_check):
+    if len(base_secrets) != len(secrets_to_check):
+        return False
+    for word in secrets_to_check:
+        for letter in word:
+            for secret in base_secrets:
+                matched_len = 0
+                for s_letter in secret:
+                    if s_letter == letter:
+                        matched_len += 1
+                if matched_len < len(secret) - 1:
+                    return False
+    return True
+
+
 def create_account(user_name: str, password: str, secret_words: list):
     def check(pwd_to_check: str, secrets_to_check: list):
-        matched_secrets = [i for i in secrets_to_check if i in secret_words]
-        if password != pwd_to_check or len(secret_words) != len(secrets_to_check):
+        if password != pwd_to_check or not are_secrets_valid(secret_words, secrets_to_check):
             return False
-        elif len(matched_secrets) >= len(secret_words)-1:
-            return True
-        else:
-            return False
+        return True
     validate_pwd(password)
     return check
